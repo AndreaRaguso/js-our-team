90d804d211067f1f21ffbbddc138bd7c17774321
Andrea Raguso <info@andrearaguso.com>
2023-01-16 15:15:29 +0100

    Milestone 1-2 fatta

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: "Founder & CEO",
+        foto : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: "Chief Editor",
+        foto : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: "Office Manager",
+        foto : "walter-gordon-office-manager.jpg"
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: "Social Media Manager",
+        foto : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: "Developer",
+        foto : "scott-estrada-developer.jpg"
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: "Grapich Designer",
+        foto : "barbara-ramos-graphic-designer.jpg"
+    },
+];
+
+for (let i = 0; i < team.length; i++) {
+
+    console.log('PERSONA ' + i);
+
+    for (let key in team[i]){
+        console.log('-', key, team[i][key]);
+    }
+
+    console.log('----------------------------------');
+}
